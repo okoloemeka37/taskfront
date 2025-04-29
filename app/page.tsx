c4c44aa7434c0097f3ba87a0290e63e6c8250be5
okoloemeka37 <97386408+okoloemeka37@users.noreply.github.com>
2025-04-29 04:36:33 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 'use client'
 
+import { useRouter } from "next/navigation";
 import { useAuth } from "./context/AuthContext";
 import { api } from "next-laravel-apihelper";
+import { useEffect } from "react";
 
 export default function Home() {
  
+  const router=useRouter()
+
+  useEffect(() => {
+    router.push('/Dashbord')
+  }, [router])
+  
   return (
     <>
       <button className="" >logout</button>
